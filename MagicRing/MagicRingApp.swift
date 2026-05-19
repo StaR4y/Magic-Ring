8bf4222888af0ce1880fd5194e7f0e7d86c66aa9
StaR4y <star4y@origin.pw>
2026-05-19 20:36:59 +0800

    Initial Commit

---
+++ MagicRing/MagicRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicRingApp.swift
+//  MagicRing
+//
+//  Created by StaRry on 2026/5/19.
+//
+
+import SwiftUI
+
+@main
+struct MagicRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
